22106993c90e535decd8031d96373b0cc9f35121
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 04:45:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ end
 
 def long_planeteer_calls(array) # code an argument here
   # Your code here
-  array.any?
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
