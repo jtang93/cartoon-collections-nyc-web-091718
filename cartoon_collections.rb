cb5e56f52186e7607610cdafa9ea380b58f39a34
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 04:39:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array)  # code an argument here
   # Your code here
   array.each_with_index do |item, index| 
     puts "#{index+1}. #{item}"
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)  # code an argument here
+  
   # Your code here
 end
 
