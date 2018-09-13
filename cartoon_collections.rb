faabcef9dee9c86b8d9c2704b6178bd05e90c2dd
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 04:31:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each do |dwarf|
-    puts "#{dwarf}"
+  array.each_with_index do |dwarf, index| {
+    array(dward) = index    
+    puts "#{dwarf}" }
   end
 end
 
